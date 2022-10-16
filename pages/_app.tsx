a7cbe389284997b30369440217d1b809e5467678
Francesco Ceccon <francesco@ceccon.me>
2022-10-16 14:44:00 +0100

    step 3: setup starknet-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { InjectedConnector, StarknetConfig } from "@starknet-react/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const connectors = [
+    new InjectedConnector({ options: { id: "argentX" } }),
+    new InjectedConnector({ options: { id: "braavos" } }),
+  ];
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <StarknetConfig connectors={connectors}>
+        <Component {...pageProps} />
+      </StarknetConfig>
     </ChakraProvider>
   );
 }
